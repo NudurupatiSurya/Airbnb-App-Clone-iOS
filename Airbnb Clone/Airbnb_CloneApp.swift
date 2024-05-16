77741164251fc21a5a0c189354a92b37f485d6ad
Surya Nudurupati <nudurupatisurya@gmail.com>
2024-05-16 12:07:15 -0700

    Initial Commit

---
+++ Airbnb Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by Surya on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
