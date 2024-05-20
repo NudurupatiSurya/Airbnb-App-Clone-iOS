8d332ce2f0d69f6e4a88dd65da3ff203df071dc4
Surya Nudurupati <nudurupatisurya@gmail.com>
2024-05-20 13:30:01 -0700

    Completed ListingDetailsView and started working on CustomSearchView/DestinationSearchView

---
+++ Airbnb Clone/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb Clone
+//
+//  Created by Surya on 5/20/24.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
